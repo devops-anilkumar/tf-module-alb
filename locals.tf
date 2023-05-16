7eabeea3eb952e299f4a80012dce57c6ec4309bc
unknown <anilkumarravuri510@gmail.com>
2023-05-16 23:51:09 +0530

    auto push

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals {
-    SG          = var.INTERNAL ? aws_security_group.alb_private.*.id : aws_security_group.alb_public.*.id
-    SUBNET_IDS  = var.INTERNAL ? data terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
-}
+# locals {
+#     SG          = var.INTERNAL ? aws_security_group.alb_private.*.id : aws_security_group.alb_public.*.id
+#     SUBNET_IDS  = var.INTERNAL ? data terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
+# }
