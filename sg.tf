64b929f59dcee92eb4c1aa6c246cd8fb908b0801
unknown <anilkumarravuri510@gmail.com>
2023-05-17 00:39:44 +0530

    auto push

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 #CREATES SECURITY GROUP - PUBLIC
-resource "aws_security_group" "alb-public" {
+resource "aws_security_group" "alb_public" {
   count       = var.INTERNAL ? 0 : 1
   name        = "robot-${var.ENV}-public-alb-sg"
   description = "Allow public traffic"
@@ -26,7 +26,7 @@ resource "aws_security_group" "alb-public" {
 }
 
 #CREATES SECURITY GROUP - PRIVATE
-resource "aws_security_group" "alb-private" {
+resource "aws_security_group" "alb_private" {
   count       = var.INTERNAL ? 1 : 0
   name        = "robot-${var.ENV}-private-alb-sg"
   description = "Allow internal traffic"
